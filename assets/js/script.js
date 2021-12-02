e5526d92c305b3eede693de2a1b529fc0c6d5e2c
amarten <alexdmarten@gmail.com>
2021-12-02 10:07:21 -0800

    display current day and date in header with luxon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
+// set js DateTime as resuable variable using luxon
+var DateTime = luxon.DateTime;
+
+// gather the current date to be displayed to user
+var dateTime = DateTime.now();
+dateTime = dateTime.toLocaleString(DateTime.DATE_HUGE);
+console.log(dateTime);
+
 // declare variables
 var currentDayEl = $('#currentDay');
-var dateTime = DateTime.now();
-console.log(dateTime);
+
 
 // function to add current date to top of the page
 var updateCurrentDay = function() {
-    $(currentDayEl).text('test');
+    $(currentDayEl).text(dateTime);
 };
 updateCurrentDay();
