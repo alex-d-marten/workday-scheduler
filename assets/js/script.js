9564560aefbc2c02cb7d30343c20204a89b2917e
amarten <alexdmarten@gmail.com>
2021-12-03 10:45:00 -0800

    timeblock coloring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
 // set js DateTime as resuable variable using luxon
 var DateTime = luxon.DateTime;
 // gather the current date to be displayed to user
-var dateTime = DateTime.now();
-dateTime = dateTime.toLocaleString(DateTime.DATE_HUGE);
+var headerDate = DateTime.now();
+headerDate = headerDate.toLocaleString(DateTime.DATE_HUGE);
 
 // declare variables and constants
 const workTimes = ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
+const workTimeNumbers = [8,9,10,11,12,13,14,15,16,17];
 const timeBlockEl = $('#time-block');
 var currentDayEl = $('#currentDay');
 
 
 // function to add current date to top of the page
 var updateCurrentDay = function() {
-    $(currentDayEl).text(dateTime);
+    $(currentDayEl).text(headerDate);
 };
 
 
 var buildTimeBlocks = function() {
+    var hourTime = DateTime.now().hour;
+
     for (var i=0; i < workTimes.length; i++) {
         const contentSection = $('<section>');
         contentSection.addClass('row');
 
         const hourDiv = $('<div>')
-        .addClass('hour col d-flex flex-column justify-content-center text-center')
-        .text(workTimes[i]);
+            .addClass('hour col d-flex flex-column justify-content-center text-center')
+            .text(workTimes[i]);
         contentSection.append(hourDiv);
 
-        const workdayContentDiv = $('<div>')
-            .addClass('col-10 future d-flex flex-column justify-content-center')
-            .text('Fil in your content here!')
+        const workdayContentDiv = $('<div>').addClass('col-10 d-flex flex-column justify-content-center content');
+
+        // const contentUl = $('<ul>')
+        //     .addClass('flex-column')
+        //     .text("test");
+        // workdayContentDiv.append(contentUl);
+
         contentSection.append(workdayContentDiv);
 
         const btnElement = $('<button>')
@@ -39,8 +46,24 @@ var buildTimeBlocks = function() {
 
         timeBlockEl.append(contentSection);
 
+        // color timeblocks according to hour of the day
+        if(workTimeNumbers[i] === hourTime) {
+            workdayContentDiv.addClass('present');
+        } 
+        else if (workTimeNumbers[i] < hourTime) {
+            workdayContentDiv.addClass('past');
+        }
+        else {
+            workdayContentDiv.addClass('future');
+        }
+        
     } 
-}
+};
+
+// edit schedule
+$('.content').on('click', 'div', function() {
+    console.log(this);
+})
 
 buildTimeBlocks();
 updateCurrentDay();
